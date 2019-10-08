5b773cec67b2095716f6c01bcf1d690f7fe7d6a7
Trent Mattner <trent.mattner@adelaide.edu.au>
2019-10-09 08:52:48 +1030

    Forgot to update semtex_mesh.m

--- semtex_mesh.m
+++ semtex_mesh.m
@@ -2,11 +2,9 @@ clear all
 close all
 
 mshfile = '/home/tmattner/Research/Nose_shaping/RunFDIP/semtex094.msh'
-fldfile = '/home/tmattner/Research/Nose_shaping/RunFDIP/semtex094.rst'
 
-data = semtex(mshfile,fldfile)
+data = semtex(mshfile)
 data.meshplot
 view([0 90])
 axis equal
 print -djpeg temp.jpg
-data.delete
